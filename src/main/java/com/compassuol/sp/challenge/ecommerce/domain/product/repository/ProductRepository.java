d3ceb33f5dc88e0f4a7b11a7702f236688eaade2
quasemago <brunoronningfn@gmail.com>
2024-01-22 17:10:29 -0400

    feat: Criado a classe repositório do domínio Product.

---
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/domain/product/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.compassuol.sp.challenge.ecommerce.domain.product.repository;
+
+import com.compassuol.sp.challenge.ecommerce.domain.product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
