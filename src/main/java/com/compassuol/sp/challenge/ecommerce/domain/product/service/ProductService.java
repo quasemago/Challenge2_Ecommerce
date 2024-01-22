646a7ee6539de74c9bde16f4334a70b681f70393
quasemago <brunoronningfn@gmail.com>
2024-01-22 17:10:42 -0400

    feat: Criado a classe de serviço do domínio Product.

---
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/domain/product/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.compassuol.sp.challenge.ecommerce.domain.product.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+}
