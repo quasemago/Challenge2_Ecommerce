b5ca5d16d4f8fdcd4ff92b0eeebf3f18304c257a
quasemago <brunoronningfn@gmail.com>
2024-01-22 13:55:46 -0400

    First Commit, spring boot project created

---
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.compassuol.sp.challenge.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
