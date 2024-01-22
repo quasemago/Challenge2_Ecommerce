8f00b8a23f9079469e1d39db3468bbd9d14f2042
quasemago <brunoronningfn@gmail.com>
2024-01-22 17:13:03 -0400

    feat: Faz a injeção do repositório na classe de serviço para uso posterior nas regras de negócio.

--- src/main/java/com/compassuol/sp/challenge/ecommerce/domain/product/service/ProductService.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/domain/product/service/ProductService.java
@@ -1,9 +1,11 @@
 package com.compassuol.sp.challenge.ecommerce.domain.product.service;
 
+import com.compassuol.sp.challenge.ecommerce.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+    private final ProductRepository productRepository;
 }
