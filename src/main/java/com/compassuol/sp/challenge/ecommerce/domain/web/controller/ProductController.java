efc932a1607b26c2cda467c874479842be7efd77
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-01-23 17:53:55 -0300

    fix: import lombok e ProductService

--- src/main/java/com/compassuol/sp/challenge/ecommerce/domain/web/controller/ProductController.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/domain/web/controller/ProductController.java
@@ -1,17 +1,11 @@
 package com.compassuol.sp.challenge.ecommerce.domain.web.controller;
 
-
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.compassuol.sp.challenge.ecommerce.domain.product.model.Product;
-
+import com.compassuol.sp.challenge.ecommerce.domain.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 
 @RequiredArgsConstructor
