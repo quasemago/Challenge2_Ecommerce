45d0abe9bee3284691b4044c85c18d024124f6ce
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-01-23 16:53:08 -0300

    feat/productController

---
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/domain/web/controller/ProductController.java
@@ -0,0 +1,55 @@
+package com.compassuol.sp.challenge.ecommerce.domain.web.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.compassuol.sp.challenge.ecommerce.domain.product.model.Product;
+
+import com.compassuol.sp.challenge.ecommerce.domain.web.dto.ProductRequestDto;
+
+@RequireArgsConstructor 
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById() {
+       return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct() {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct() {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct() {
+        return null;
+    }
+}
+
