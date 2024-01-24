24213c9628507afe44121cb6967ea820f36d9cfc
quasemago <brunoronningfn@gmail.com>
2024-01-23 20:10:43 -0400

    feat: Mapeia a classe do controller para a documentação por meio do Swagger.

--- src/main/java/com/compassuol/sp/challenge/ecommerce/domain/web/controller/ProductController.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/domain/web/controller/ProductController.java
@@ -5,15 +5,20 @@ import com.compassuol.sp.challenge.ecommerce.domain.product.service.ProductServi
 import com.compassuol.sp.challenge.ecommerce.domain.web.dto.ProductCreateDto;
 import com.compassuol.sp.challenge.ecommerce.domain.web.dto.ProductResponseDto;
 import com.compassuol.sp.challenge.ecommerce.domain.web.dto.mapper.ProductMapper;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 
+@Tag(name = "Products API", description = "Contém as operações relativas aos recursos de interação com o domínio produtos.")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/products")
