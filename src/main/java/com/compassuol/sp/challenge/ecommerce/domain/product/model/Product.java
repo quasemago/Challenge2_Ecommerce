7c020d0a597c83752c3e170e4e02bdfa1c6e95ef
Kober <pedrokoberstain2@gmail.com>
2024-01-24 20:47:25 -0300

    feat: Implementação da camada de teste do repository (Metódo create).

--- src/main/java/com/compassuol/sp/challenge/ecommerce/domain/product/model/Product.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/domain/product/model/Product.java
@@ -1,6 +1,7 @@
 package com.compassuol.sp.challenge.ecommerce.domain.product.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -15,19 +16,22 @@ import java.util.Objects;
 @Setter
 @ToString
 @Entity
+@Builder
 @Table(name = "products")
 public class Product implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
     @Column(name = "name", unique = true, nullable = false, length = 120)
     private String name;
-    @Column(name = "value", nullable = false, columnDefinition = "DECIMAL(10,2)")
+    @Column(name = "price", nullable = false, columnDefinition = "DECIMAL(10,2)")
     @Positive
     private BigDecimal value;
     @Column(name = "description", nullable = false, length = 256)
     @Size(min = 10)
     private String description;
+
 
     @Override
     public boolean equals(Object o) {
---
+++ src/test/java/com/compassuol/sp/challenge/ecommerce/domain/product/ProductRepositoryTest.java
@@ -0,0 +1,76 @@
+package com.compassuol.sp.challenge.ecommerce.domain.product;
+
+
+import com.compassuol.sp.challenge.ecommerce.domain.product.model.Product;
+import com.compassuol.sp.challenge.ecommerce.domain.product.repository.ProductRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.math.BigDecimal;
+import java.util.stream.Stream;
+
+import static com.compassuol.sp.challenge.ecommerce.domain.product.common.ProductsConstants.VALID_PRODUCT;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@ActiveProfiles("test")
+@DataJpaTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @AfterEach
+    public void afterEach() {
+        VALID_PRODUCT.setId(null);
+    }
+    @Test
+    public void createProduct_WithValidData_ReturnsProduct() {
+        Product savedProduct = productRepository.save(VALID_PRODUCT);
+
+        Product sutProduct = testEntityManager.find(Product.class, savedProduct.getId());
+
+        assertThat(sutProduct).isNotNull();
+        assertThat(sutProduct.getName()).isEqualTo(VALID_PRODUCT.getName());
+        assertThat(sutProduct.getDescription()).isEqualTo(VALID_PRODUCT.getDescription());
+        assertThat(sutProduct.getValue()).isEqualTo(VALID_PRODUCT.getValue());
+    }
+
+    private static Stream<Arguments> providesInvalidProducts() {
+        return Stream.of(
+                Arguments.of(Product.builder().name("").description("Description Valid").value(BigDecimal.valueOf(10.0)).build()),
+                Arguments.of(Product.builder().name("Product Valid").description("").value(BigDecimal.valueOf(10.0)).build()),
+                Arguments.of(Product.builder().name("Product Valid").description("Description Valid").value(BigDecimal.valueOf(-10.0)).build())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("providesInvalidProducts")
+    public void createProduct_WithInvalidData_ReturnsProduct(Product product) {
+        assertThatThrownBy(() -> productRepository.save(product))
+                .isInstanceOf(Exception.class);
+    }
+
+    @Test
+    public void createProduct_WithExistingName_ThrowsException() {
+        final Product planet = testEntityManager.persistFlushFind(VALID_PRODUCT);
+        testEntityManager.detach(planet);
+        planet.setId(null);
+
+        assertThatThrownBy(() -> productRepository.save(planet))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+
+}
