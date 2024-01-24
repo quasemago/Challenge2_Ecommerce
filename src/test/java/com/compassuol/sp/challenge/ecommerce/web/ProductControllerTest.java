ab42cbcd7144677721760d4c893337364ca546b0
quasemago <brunoronningfn@gmail.com>
2024-01-24 16:34:51 -0400

    feat: Testes do endpoint 'getAllProducts' da API Product.

--- src/test/java/com/compassuol/sp/challenge/ecommerce/web/ProductControllerTest.java
+++ src/test/java/com/compassuol/sp/challenge/ecommerce/web/ProductControllerTest.java
@@ -14,9 +14,13 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -47,6 +51,12 @@ public class ProductControllerTest {
         dto.setDescription(p.getDescription());
         dto.setValue(p.getValue());
         return dto;
+    }
+
+    private static List<ProductResponseDto> toResponseDtoList(List<Product> products) {
+        return products.stream()
+                .map(ProductControllerTest::toResponseDto)
+                .collect(Collectors.toList());
     }
 
     @Test
@@ -83,5 +93,45 @@ public class ProductControllerTest {
                                 .content(objectMapper.writeValueAsString(createProductDto(invalidProduct)))
                 )
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void getAllProducts_ReturnsProductList() throws Exception {
+        final List<Product> products = List.of(
+                Product.builder()
+                        .id(1L).name("Product 1").description("Description 1").value(BigDecimal.valueOf(10.0))
+                        .build(),
+                Product.builder()
+                        .id(2L).name("Product 2").description("Description 2").value(BigDecimal.valueOf(20.0))
+                        .build()
+        );
+
+        when(productService.getAllProducts()).thenReturn(products);
+        final List<ProductResponseDto> listDto = toResponseDtoList(products);
+
+        mockMvc.perform(
+                        get("/products")
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$.length()").value(listDto.size()))
+                .andExpectAll(
+                        jsonPath("$[0].name").value(listDto.get(0).getName()),
+                        jsonPath("$[0].description").value(listDto.get(0).getDescription()),
+                        jsonPath("$[0].value").value(listDto.get(0).getValue()),
+                        jsonPath("$[1].name").value(listDto.get(1).getName()),
+                        jsonPath("$[1].description").value(listDto.get(1).getDescription()),
+                        jsonPath("$[1].value").value(listDto.get(1).getValue())
+                );
+    }
+
+    @Test
+    public void getAllProducts_ReturnsEmptyList() throws Exception {
+        when(productService.getAllProducts()).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(
+                        get("/products"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isEmpty());
     }
 }
