f669d2194545109734ce4442aa4b66f8c356916f
quasemago <brunoronningfn@gmail.com>
2024-01-25 13:34:23 -0400

    refactor: Refatora o nome de alguns métodos e outras boas práticas.

--- src/test/java/com/compassuol/sp/challenge/ecommerce/domain/product/ProductServiceTest.java
+++ src/test/java/com/compassuol/sp/challenge/ecommerce/domain/product/ProductServiceTest.java
@@ -10,11 +10,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+
 import static com.compassuol.sp.challenge.ecommerce.domain.product.common.ProductsConstants.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -29,38 +31,38 @@ public class ProductServiceTest {
     private ProductRepository productRepository;
 
     @Test
-    public void create_WithValidData_ReturnsProduct(){
+    public void createProduct_WithValidData_ReturnsProduct() {
         when(productRepository.save(VALID_PRODUCT)).thenReturn(VALID_PRODUCT);
         Product sut = productService.create(VALID_PRODUCT);
+        assertThat(sut).isNotNull();
         assertThat(sut).isEqualTo(VALID_PRODUCT);
-        verify(productRepository).save(VALID_PRODUCT);
+        verify(productRepository, times(1)).save(VALID_PRODUCT);
     }
+
     @Test
-    public void create_WithInvalidData_ThrowsException(){
+    public void createProduct_WithInvalidData_ThrowsException() {
         when(productRepository.save(INVALID_PRODUCT)).thenThrow(RuntimeException.class);
         assertThatThrownBy(() -> productService.create(INVALID_PRODUCT)).isInstanceOf(RuntimeException.class);
-        verify(productRepository).save(INVALID_PRODUCT);
-
-
+        verify(productRepository, times(1)).save(INVALID_PRODUCT);
     }
 
     @Test
-    public void getProductById_WithExistingId_ReturnsProduct(){
+    public void getProductById_WithExistingId_ReturnsProduct() {
         when(productRepository.findById(1L)).thenReturn(Optional.of(PRODUCT_1));
         Product sut = productService.getProductById(1L);
         assertThat(sut).isEqualTo(PRODUCT_1);
-        verify(productRepository).findById(1L);
+        verify(productRepository, times(1)).findById(1L);
     }
+
     @Test
-    public void getProductById_WithUnexistingId_ThrowsEntityNotFoundException(){
+    public void getProductById_WithNonExistingId_ThrowsEntityNotFoundException() {
         when(productRepository.findById(1L)).thenReturn(Optional.empty());
-        assertThatThrownBy(() ->  productService.getProductById(1L)).isInstanceOf(EntityNotFoundException.class);
-        verify(productRepository).findById(1L);
-
+        assertThatThrownBy(() -> productService.getProductById(1L)).isInstanceOf(EntityNotFoundException.class);
+        verify(productRepository, times(1)).findById(1L);
     }
 
     @Test
-    public void getAllProducts_ReturnAllProducts(){
+    public void getAllProducts_ReturnAllProducts() {
         List<Product> mockProducts = Arrays.asList(PRODUCT_1, PRODUCT_2);
         when(productRepository.findAll()).thenReturn(mockProducts);
         List<Product> sut = productService.getAllProducts();
@@ -69,33 +71,34 @@ public class ProductServiceTest {
         assertThat(sut).hasSize(2);
         assertThat(sut).containsExactlyInAnyOrder(PRODUCT_1, PRODUCT_2);
 
-        verify(productRepository).findAll();
+        verify(productRepository, times(1)).findAll();
 
     }
+
     @Test
-    public void getAllProducts_ReturnNoProducts(){
+    public void getAllProducts_ReturnNoProducts() {
         when(productRepository.findAll()).thenReturn(Collections.emptyList());
         List<Product> sut = productService.getAllProducts();
 
         assertThat(sut).isEmpty();
 
-        verify(productRepository).findAll();
-
+        verify(productRepository, times(1)).findAll();
     }
+
     @Test
-    public void deleteProduct_WithExistingId_doesNotThrowAnyException(){
+    public void deleteProduct_WithExistingId_DoesNotThrowAnyException() {
         Long productId = 1L;
         when(productRepository.existsById(productId)).thenReturn(true);
 
         assertThatCode(() -> productService.deleteProduct(productId)).doesNotThrowAnyException();
 
-        verify(productRepository).existsById(productId);
-        verify(productRepository).deleteById(productId);
+        verify(productRepository, times(1)).existsById(productId);
+        verify(productRepository, times(1)).deleteById(productId);
 
     }
+
     @Test
     public void deleteProduct_ProductDoesNotExist_ThrowsEntityNotFoundException() {
-
         Long productId = 1L;
         when(productRepository.existsById(productId)).thenReturn(false);
 
@@ -103,13 +106,12 @@ public class ProductServiceTest {
                 .isInstanceOf(EntityNotFoundException.class)
                 .hasMessage("Não existe o produto com o Id: " + productId);
 
-        verify(productRepository).existsById(productId);
+        verify(productRepository, times(1)).existsById(productId);
         verify(productRepository, never()).deleteById(productId);
     }
 
     @Test
     public void updateProduct_WithExistingId_ReturnsUpdatedProduct() {
-
         Long productId = 3L;
         when(productRepository.findById(productId)).thenReturn(Optional.of(EXISTING_PRODUCT));
         when(productRepository.save(EXISTING_PRODUCT)).thenReturn(UPDATED_PRODUCT);
@@ -119,25 +121,23 @@ public class ProductServiceTest {
         assertThat(sut).isNotNull();
         assertThat(sut).isEqualTo(UPDATED_PRODUCT);
         assertThat(EXISTING_PRODUCT.getName()).isEqualTo("New Name");
-        assertThat(EXISTING_PRODUCT.getValue()).isEqualTo(new BigDecimal(20.0));
+        assertThat(EXISTING_PRODUCT.getValue()).isEqualTo(BigDecimal.valueOf(20.0));
         assertThat(EXISTING_PRODUCT.getDescription()).isEqualTo("New Description");
 
-        verify(productRepository).findById(productId);
-        verify(productRepository).save(EXISTING_PRODUCT);
+        verify(productRepository, times(1)).findById(productId);
+        verify(productRepository, times(1)).save(EXISTING_PRODUCT);
     }
 
     @Test
     public void updateProduct_ProductDoesNotExist_ThrowsEntityNotFoundException() {
-
         Long productId = 3L;
         when(productRepository.findById(productId)).thenReturn(Optional.empty());
-
 
         assertThatThrownBy(() -> productService.update(UPDATED_PRODUCT, productId))
                 .isInstanceOf(EntityNotFoundException.class)
                 .hasMessage("Não existe o produto com o Id: " + productId);
 
-        verify(productRepository).findById(productId);
+        verify(productRepository, times(1)).findById(productId);
         verify(productRepository, never()).save(any(Product.class));
     }
 }
