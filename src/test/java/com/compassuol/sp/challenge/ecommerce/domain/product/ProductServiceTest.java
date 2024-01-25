597b21fc659e8c9fbfdf071c261a7ad524bb40bc
Mayke <mayke000000000@gmail.com>
2024-01-25 09:49:02 -0300

    feat: Testes do método getProductById

--- src/test/java/com/compassuol/sp/challenge/ecommerce/domain/product/ProductServiceTest.java
+++ src/test/java/com/compassuol/sp/challenge/ecommerce/domain/product/ProductServiceTest.java
@@ -4,13 +4,18 @@ package com.compassuol.sp.challenge.ecommerce.domain.product;
 import com.compassuol.sp.challenge.ecommerce.domain.product.model.Product;
 import com.compassuol.sp.challenge.ecommerce.domain.product.repository.ProductRepository;
 import com.compassuol.sp.challenge.ecommerce.domain.product.service.ProductService;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static com.compassuol.sp.challenge.ecommerce.domain.product.common.ProductsConstants.INVALID_PRODUCT;
-import static com.compassuol.sp.challenge.ecommerce.domain.product.common.ProductsConstants.VALID_PRODUCT;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static com.compassuol.sp.challenge.ecommerce.domain.product.common.ProductsConstants.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -36,7 +41,17 @@ public class ProductServiceTest {
 
     }
 
+    @Test
+    public void getProductById_WithExistingId_ReturnsProduct(){
+        when(productRepository.findById(1L)).thenReturn(Optional.of(PRODUCT_WITH_ID));
+        Product sut = productService.getProductById(1L);
+        assertThat(sut).isEqualTo(PRODUCT_WITH_ID);
+    }
+    @Test
+    public void getProductById_WithUnexistingId_EntityNotFoundException(){
+        when(productRepository.findById(1L)).thenReturn(Optional.empty());
+       assertThatThrownBy(() ->  productService.getProductById(1L)).isInstanceOf(EntityNotFoundException.class);
 
-
+    }
 
 }
