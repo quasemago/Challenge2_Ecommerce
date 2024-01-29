09b484ad28105bcc97aee7940f81465dcff544fd
luisafkroth <luisafkroth@gmail.com>
2024-01-29 16:22:36 -0300

    feat: Criação da classe OrdersService

---
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/domain/product/service/OrdersService.java
@@ -0,0 +1,10 @@
+package com.compassuol.sp.challenge.ecommerce.domain.product.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrdersService {
+
+}
