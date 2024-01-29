adb0eb30bc59bd4207fe2ac0ed6592abbed628f6
Kober <pedrokoberstain2@gmail.com>
2024-01-29 17:08:25 -0400

    feat: Injetando o orderRepository no OrderService para implementação posterior.

--- src/main/java/com/compassuol/sp/challenge/ecommerce/domain/order/service/OrderService.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/domain/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.compassuol.sp.challenge.ecommerce.domain.order.service;
 
+import com.compassuol.sp.challenge.ecommerce.domain.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderService {
 
+    private final OrderRepository orderRepository;
 }
