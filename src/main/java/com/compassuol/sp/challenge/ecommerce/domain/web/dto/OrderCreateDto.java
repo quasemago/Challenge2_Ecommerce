0053c2fb073c4ce89e9bf36418e98b832f05c9a6
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-01-29 19:00:57 -0300

    feat:criação classe OrderCreateDto

---
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/domain/web/dto/OrderCreateDto.java
@@ -0,0 +1,26 @@
+package com.compassuol.sp.challenge.ecommerce.domain.web.dto;
+
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+
+public class OrderCreateDto {
+    
+    private Long id;
+    private List<ProductResponseDTO> products;
+    private AddressDTO address;
+    private String paymentMethod;
+    private BigDecimal subtotalValue;
+    private BigDecimal discount;
+    private BigDecimal totalValue;
+    private LocalDateTime createdDate;
+    private String status;
+}
