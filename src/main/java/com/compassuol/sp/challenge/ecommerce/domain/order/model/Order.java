73ac3e3d3a4c27927e2f4474a0481e9cd759f972
quasemago <brunoronningfn@gmail.com>
2024-01-31 19:44:44 -0400

    feat: Adiciona validação na entidade modelo 'Order' para garantir que a lista de produtos não esteja vazia.

--- src/main/java/com/compassuol/sp/challenge/ecommerce/domain/order/model/Order.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/domain/order/model/Order.java
@@ -2,8 +2,8 @@ package com.compassuol.sp.challenge.ecommerce.domain.order.model;
 
 import com.compassuol.sp.challenge.ecommerce.domain.order.enums.OrderStatus;
 import com.compassuol.sp.challenge.ecommerce.domain.order.enums.PaymentMethod;
-import com.compassuol.sp.challenge.ecommerce.domain.product.model.Product;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -22,29 +22,41 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "order", fetch = FetchType.EAGER)
+    @NotEmpty
     private List<OrderProduct> products;
+
     @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "address_id")
+    @JoinColumn(name = "address_id", nullable = false)
     private Address address;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "payment_method", nullable = false)
     private PaymentMethod paymentMethod;
+
     @Column(name = "subtotal_value", nullable = false, columnDefinition = "DECIMAL(10,2)")
     private BigDecimal subtotalValue;
+
     @Column(name = "discount", nullable = false, columnDefinition = "DECIMAL(10,2)")
     private BigDecimal discount;
+
     @Column(name = "total_value", nullable = false, columnDefinition = "DECIMAL(10,2)")
     private BigDecimal totalValue;
+
     @Column(name = "created_date", nullable = false)
     private LocalDateTime createdDate;
+
     @Column(name = "update_date")
     private LocalDateTime updateDate;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "status", nullable = false)
     private OrderStatus status;
+
     @Column(name = "cancel_reason", length = 256)
     private String cancelReason;
+
     @Column(name = "cancel_date")
     private LocalDateTime cancelDate;
 }
