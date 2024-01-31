ad3c930412bbcbf7560aad5cf4e50d0a8d3b045c
quasemago <brunoronningfn@gmail.com>
2024-01-31 19:45:21 -0400

    refactor: Move a classe 'ProductConstants' para outro diretório, a fim de ser comum aos testes.

--- src/test/java/com/compassuol/sp/challenge/ecommerce/common/ProductConstants.java
+++ src/test/java/com/compassuol/sp/challenge/ecommerce/common/ProductConstants.java
@@ -1,4 +1,4 @@
-package com.compassuol.sp.challenge.ecommerce.domain.product.common;
+package com.compassuol.sp.challenge.ecommerce.common;
 
 import com.compassuol.sp.challenge.ecommerce.domain.product.model.Product;
 import lombok.AccessLevel;
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class ProductsConstants {
+public class ProductConstants {
     public static final Product VALID_PRODUCT = Product.builder()
             .name("Product Valid").description("Description Valid").value(BigDecimal.valueOf(10.0))
             .build();
