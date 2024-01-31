d3a715c726d83b217986e5b0dbdaaa11ebc1f1b9
Franciele Dalarosa <140400674+francieledalarosa@users.noreply.github.com>
2024-01-31 14:35:46 -0300

    feat:criação de funções service para cancelamento de pedido, verificando as regras de negócios, permitindo que apenas pedidos com data igual ou inferior a 90 dias sejam cancelados; Adiconais na classe expection pra tratamento de excesão

---
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/domain/order/exception/OrderCancellationNotAllowedException.java
@@ -0,0 +1,7 @@
+package com.compassuol.sp.challenge.ecommerce.domain.order.exception;
+
+public class OrderCancellationNotAllowedException extends RuntimeException {
+    public OrderCancellationNotAllowedException(String message) {
+        super(message);
+    }
+}
