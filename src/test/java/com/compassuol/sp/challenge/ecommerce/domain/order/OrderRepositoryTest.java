2a2b1003b9ca17cb87047b32bf9a97defef65dd3
quasemago <brunoronningfn@gmail.com>
2024-01-31 14:56:17 -0400

    feat: Criada classe de testes do repositório da API 'Order'.

---
+++ src/test/java/com/compassuol/sp/challenge/ecommerce/domain/order/OrderRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.compassuol.sp.challenge.ecommerce.domain.order;
+
+import com.compassuol.sp.challenge.ecommerce.domain.order.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@ActiveProfiles("test")
+@DataJpaTest
+public class OrderRepositoryTest {
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private OrderRepository orderRepository;
+}
