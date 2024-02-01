47ae46395cb7969020e82aa5b63480edafb61bcb
quasemago <brunoronningfn@gmail.com>
2024-02-01 19:43:09 -0400

    feat: Atualizado as datas da resposta do pedido 'OrderResponseDto' para o formato ISO 8601.

--- src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/OrderResponseDto.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/OrderResponseDto.java
@@ -1,5 +1,6 @@
 package com.compassuol.sp.challenge.ecommerce.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,10 +21,12 @@ public class OrderResponseDto {
     private BigDecimal subtotalValue;
     private BigDecimal discount;
     private BigDecimal totalValue;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime createdDate;
     private String status;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String cancelReason;
     @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime cancelDate;
 }
