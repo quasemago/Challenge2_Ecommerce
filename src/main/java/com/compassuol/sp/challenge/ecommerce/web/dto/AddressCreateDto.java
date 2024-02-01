3ae80d18e8b543ecddec56c8aff04c9d538a84ff
quasemago <brunoronningfn@gmail.com>
2024-02-01 19:56:39 -0400

    feat: Adiciona validação da formatação do código postal (CEP) no Dto do endereço ao criar o pedido.

--- src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/AddressCreateDto.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/AddressCreateDto.java
@@ -2,9 +2,11 @@ package com.compassuol.sp.challenge.ecommerce.web.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
@@ -15,5 +17,7 @@ public class AddressCreateDto {
     @NotBlank
     private String complement;
     @NotBlank
+    @Length(min = 8, max = 8, message = "O código postal deve ter 8 caracteres.")
+    @Pattern(regexp = "^[0-9]{8}$", message = "O código postal deve conter apenas números.")
     private String postalCode;
 }
