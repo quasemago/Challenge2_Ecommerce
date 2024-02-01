f5b94a7622593ae8f8c08d60f551210f9ec9bdf3
quasemago <brunoronningfn@gmail.com>
2024-02-01 19:57:18 -0400

    feat: Remove exceções customizadas do OpenFeign não utilizadas, visto que agora o formato é tratado diretamente no Bean Validation.

--- src/main/java/com/compassuol/sp/challenge/ecommerce/domain/order/exception/OpenFeignBadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.compassuol.sp.challenge.ecommerce.domain.order.exception;
-
-public class OpenFeignBadRequestException extends RuntimeException{
-    public OpenFeignBadRequestException(String message) {
-        super(message);
-    }
-}
