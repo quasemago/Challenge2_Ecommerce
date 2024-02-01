41916c000447e67549d3a42634d0b2fea0e97581
Mayke <mayke000000000@gmail.com>
2024-02-01 17:00:38 -0300

    feat: Teste do repository updateOrder.

--- src/test/java/com/compassuol/sp/challenge/ecommerce/domain/order/OrderRepositoryTest.java
+++ src/test/java/com/compassuol/sp/challenge/ecommerce/domain/order/OrderRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.compassuol.sp.challenge.ecommerce.domain.order;
 
+import com.compassuol.sp.challenge.ecommerce.domain.order.enums.OrderStatus;
 import com.compassuol.sp.challenge.ecommerce.domain.order.enums.PaymentMethod;
 import com.compassuol.sp.challenge.ecommerce.domain.order.model.Order;
 import com.compassuol.sp.challenge.ecommerce.domain.order.repository.OrderRepository;
@@ -8,9 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.LocalDateTime;
 
 import static com.compassuol.sp.challenge.ecommerce.common.OrderUtils.generateInvalidOrder;
 import static com.compassuol.sp.challenge.ecommerce.common.OrderUtils.generateValidOrder;
+import static com.compassuol.sp.challenge.ecommerce.common.ProductConstants.EXISTING_PRODUCT;
 import static com.compassuol.sp.challenge.ecommerce.common.ProductConstants.VALID_PRODUCT;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -44,4 +49,32 @@ public class OrderRepositoryTest {
         assertThatThrownBy(() -> orderRepository.save(sutOrder))
                 .isInstanceOf(Exception.class);
     }
+    @Sql("/sql/products/insert_products.sql")
+    @Test
+    public void updateOrder_WithExistingOrderId_ReturnsUpdatedOrder() {
+
+        final Order validOrder = generateValidOrder(PaymentMethod.PIX, EXISTING_PRODUCT);
+        validOrder.setStatus(OrderStatus.CONFIRMED);
+        final Order savedOrder = testEntityManager.persistAndFlush(validOrder);
+
+
+        final Order updatedOrder = new Order();
+        updatedOrder.setId(savedOrder.getId());
+        updatedOrder.setStatus(OrderStatus.SENT);
+        updatedOrder.setUpdateDate(LocalDateTime.now());
+
+
+        final Order updatedEntity = orderRepository.save(updatedOrder);
+
+        final Order retrievedOrder = testEntityManager.find(Order.class, savedOrder.getId());
+
+        assertThat(retrievedOrder).isNotNull();
+        assertThat(retrievedOrder.getStatus()).isEqualTo(OrderStatus.SENT);
+        assertThat(retrievedOrder.getUpdateDate()).isNotNull();
+
+        assertThat(updatedEntity).isNotNull();
+        assertThat(updatedEntity.getStatus()).isEqualTo(OrderStatus.SENT);
+        assertThat(updatedEntity.getUpdateDate()).isEqualTo(retrievedOrder.getUpdateDate());
+    }
+
 }
